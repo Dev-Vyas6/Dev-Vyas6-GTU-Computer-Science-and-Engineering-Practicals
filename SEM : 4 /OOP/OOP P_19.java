5b94a31c50693ee2780bae5a52044bffe7c38d81
Dev Vyas <102415609+Dev-Vyas6@users.noreply.github.com>
2022-10-08 21:36:52 +0530

    Create OOP P_19.java

---
+++ SEM : 4 /OOP/OOP P_19.java
@@ -0,0 +1,27 @@
+// AIM : Write a program that displays the color of a circle as red when the  mouse button is pressed and as blue when the mouse button is released.
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class P19 extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+    double width = 400;
+    double height = 400;
+    Circle c = new Circle(width / 2, height / 2, Math.min(width, height) / 10, Color.WHITE);
+    c.setStroke(Color.BLACK);
+    StackPane pane = new StackPane(c);
+    primaryStage.setScene(new Scene(pane, width, height));
+    pane.setOnMousePressed(e -> c.setFill(Color.BLACK));
+    pane.setOnMouseReleased(e -> c.setFill(Color.WHITE));
+    primaryStage.setTitle("Click circle..");
+    primaryStage.show();
+  }
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+}
