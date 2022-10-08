4698e171b3ac55f40832501324e10ae43812fdfa
Dev Vyas <102415609+Dev-Vyas6@users.noreply.github.com>
2022-10-08 20:48:41 +0530

    Create OOP P3.java

---
+++ SEM : 4 /OOP/OOP P3.java
@@ -0,0 +1,22 @@
+// AIM:- Write a program that reads a number in meters, converts it to feet, and displays the result. 
+
+import java.util.Scanner;
+
+public class Practical3 {
+
+  public static void main(String[] args)
+
+  {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter Value in Meters :");
+
+    double meter = input.nextDouble();
+
+    double feet = meter * 3.28084;
+
+    System.out.print(meter + " Meters = " + feet + " Feets");
+
+  }
+}
