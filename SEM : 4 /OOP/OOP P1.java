b0bc2026c22483f062f4cddb6487232853643ac7
Dev Vyas <102415609+Dev-Vyas6@users.noreply.github.com>
2022-10-08 20:43:43 +0530

    Create OOP P1.java

---
+++ SEM : 4 /OOP/OOP P1.java
@@ -0,0 +1,9 @@
+//AIM:- Write a Program that displays Welcome to Java, Learning Java Now and Programming is fun. 
+
+public class Practical1
+{
+  public static void main(String[] args) 
+ {
+    System.out.println("Welcome to Java, Learning Java Now and Programming is fun.");
+ } 
+}
